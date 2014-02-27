34dd89cb2d8854805dd6e7200ba572a4b6caf158
Igor <igor.rafael@gmail.com>
2014-02-27 14:21:40 -0300

    Improved CSS

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -1,4 +1,5 @@
-angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
+angular.module('toggle-switch', ['ng'])
+.directive('toggleSwitch', function () {
   return {
     restrict: 'EA',
     replace: true,
@@ -6,10 +7,13 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
       model: '=',
       disabled: '@',
       onLabel: '@',
+      onColor: '@',
       offLabel: '@',
-      knobLabel: '@'
+      offColor: '@',
+      knobLabel: '@',
+      onAfterChange: '@'
     },
-    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div class="switch-animate" ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left" ng-bind="onLabel"></span><span class="knob" ng-bind="knobLabel"></span><span class="switch-right" ng-bind="offLabel"></span></div></div>',
+    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div class="switch-animate" ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-{{onColor || \'primary\'}}" ng-bind="onLabel"></span><span class="knob" ng-bind="knobLabel"></span><span class="switch-{{offColor || \'default\'}}" ng-bind="offLabel"></span></div></div>',
     controller: function($scope) {
       $scope.toggle = function toggle() {
         if(!$scope.disabled) {
@@ -18,10 +22,21 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
       };
     },
     compile: function(element, attrs) {
-      if (!attrs.onLabel) { attrs.onLabel = 'On'; }
-      if (!attrs.offLabel) { attrs.offLabel = 'Off'; }
-      if (!attrs.knobLabel) { attrs.knobLabel = '\u00a0'; }
-      if (!attrs.disabled) { attrs.disabled = false; }
+      if (!attrs.onLabel) { 
+        attrs.onLabel = 'On';
+      }
+      
+      if (!attrs.offLabel) {
+        attrs.offLabel = 'Off';
+      }
+      
+      if (!attrs.knobLabel) {
+        attrs.knobLabel = '\u00a0';
+      }
+
+      if (!attrs.disabled) {
+        attrs.disabled = false;
+      }
     },
   };
 });
